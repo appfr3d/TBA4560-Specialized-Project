f1944797683b49afe1769df1d5cf8ac8db2f3dbd
Alfred Lieth Årøe <Alfred.aroe97@gmail.com>
2021-12-19 11:10:13 +0100

    💄 Visualized results with images as well now

--- Pointnet_Pointnet2_pytorch/save_visualizations_results.py
+++ Pointnet_Pointnet2_pytorch/save_visualizations_results.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import open3d as o3d
 import laspy
+import time
 
 root = 'log/'
 result_dirs = ['log/part_seg/gt',
@@ -9,17 +10,28 @@ result_dirs = ['log/part_seg/gt',
                'log/inst_seg/gt',
                'log/inst_seg/pointnet2_inst_seg_msg_tr3d/results',
                'log/inst_seg/pointnet2_inst_seg_msg_tr3d_3/results',
-               'log/inst_seg/pointnet2_inst_seg_msg_tr3d_swap/results',
                'log/inst_seg/pointnet2_inst_seg_msg_tr3d_scale/results',
-               'log/inst_seg/pointnet2_inst_seg_msg_tr3d_swap_scale/results']
+               'log/inst_seg/pointnet2_inst_seg_msg_tr3d_swap_scale/results',
+               'log/inst_seg/pointnet2_inst_seg_msg_tr3d_swap/results',
+               'log/inst_seg/pointnet2_inst_seg_msg_tr3d_swap_scale_new/results']
+
+
+# Create window
+vis = o3d.visualization.Visualizer()
+vis.create_window(width=1000, height=1000)
+
 
 
 for dir in result_dirs:
-    fns = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+    print('Creating images for', dir, '...', end=' ')
+    if not os.path.exists(os.path.join(dir, 'images')):
+        os.makedirs(os.path.join(dir, 'images'))
 
+    fns = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+    
     for fn in fns:
         # Read file
-        file_name = os.path.join(dir, fn)
+        file_name = os.path.join(dir, fn) # 'Cross Element.las') # 
         point_cloud = laspy.read(file_name)
 
         # Get points and color
@@ -31,18 +43,38 @@ for dir in result_dirs:
         # Create o3d point cloud with color
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points)
-        pcd.colors = o3d.utility.Vector3dVector(colors/65535)
+        pcd.colors = o3d.utility.Vector3dVector(colors/255)
 
-        # Visualize
-        vis = o3d.visualization.Visualizer()
-        vis.create_window(visible=False)
         vis.add_geometry(pcd)
+        vis.get_render_option().load_from_json("visualization_viewpoint.json") # set point size
         vis.update_geometry(pcd)
         vis.poll_events()
         vis.update_renderer()
 
+        # Set viewpoint
+        ctr = vis.get_view_control()
+        ctr.change_field_of_view(60)
+        ctr.set_lookat([ -0.059999999999999998, 0.010000000000000009, -0.0050000000000000044])
+        ctr.set_up([ 0.020758910712584521, 0.86761340229561579, 0.49680584918361509 ])
+        ctr.set_zoom(0.69999999999999996)
+        ctr.set_front([ -0.29112078289923043, -0.47013475316588499, 0.83319985815516784 ])
+        
+
+        # vis.run()
+        # time.sleep(5)
+
+        # vis.get_render_option().save_to_json("visualization_viewpoint.json")
+
+        # break 
         # Save
         file_name = fn.split('.')[0]
         vis.capture_screen_image(os.path.join(
-            dir, 'results', 'images', file_name + '.png'))
-        vis.destroy_window()
+            dir, 'images', file_name + '.png'))
+        
+        # Reset
+        vis.remove_geometry(pcd)
+    # break
+    print('done!')
+        
+
+vis.destroy_window()
