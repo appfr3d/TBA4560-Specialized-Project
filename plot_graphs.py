c90579105fd0a9cb6e52c2261e090fccc29882b8
Alfred Lieth Årøe <alfred.aroe97@gmail.com>
2021-12-20 12:13:47 +0100

    💄 Plot damping graph

---
+++ plot_graphs.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 100 linearly spaced numbers
+x = np.linspace(0, 1, 100)
+
+
+# the function
+def scale(val): return 0.5*np.log10(val + 0.01) + 1
+
+
+# calculation of the function
+y = scale(x)
+
+
+# setting the axes at the centre
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+# ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# plot the function
+plt.plot(x, y, 'r')
+
+# show the plot
+plt.show()
